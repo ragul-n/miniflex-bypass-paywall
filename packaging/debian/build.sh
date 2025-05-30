a19e1ab41c8fa41eecb3fb9df4f59288f740db8d
ragul-n <natarajanragul8@gmail.com>
2025-05-30 22:19:24 +0530

    reverting to old build.sh file

--- packaging/debian/build.sh
+++ packaging/debian/build.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-set -e  # Exit on errors
-
 PKG_ARCH=$(dpkg --print-architecture)
 PKG_DATE=$(date -R)
 PKG_VERSION=$(cd /src && git describe --tags --abbrev=0 | sed 's/^v//')
 
-PKG_VERSION=$(cd /src && git describe --tags --abbrev=0 2>/dev/null | sed 's/^v//')
-if [ -z "$PKG_VERSION" ]; then
-  PKG_VERSION="0.0.1"
-fi
-
-echo "Building Miniflux $PKG_VERSION for $PKG_ARCH"
+echo "PKG_VERSION=$PKG_VERSION"
+echo "PKG_ARCH=$PKG_ARCH"
+echo "PKG_DATE=$PKG_DATE"
 
 cd /src
 
@@ -21,24 +16,23 @@ else
     CGO_ENABLED=0 make miniflux
 fi
 
-# Prepare build directory
-mkdir -p /build/debian
-cd /build
-cp /src/miniflux /build/
-cp /src/miniflux.1 /build/
-cp /src/LICENSE /build/
-cp /src/packaging/miniflux.conf /build/
-cp /src/packaging/systemd/miniflux.service /build/debian/
-cp /src/packaging/debian/* /build/debian/
-
-# Generate changelog
-dch --create --package miniflux --newversion "$PKG_VERSION" --distribution unstable \
-    --urgency=medium "Miniflux version $PKG_VERSION"
-
-# Replace architecture placeholder
-sed "s/__PKG_ARCH__/${PKG_ARCH}/g" /src/packaging/debian/control > /build/debian/control
-
-# Build package
-dpkg-buildpackage -us -uc -b
-lintian --check --color always ../*.deb
+mkdir -p /build/debian && \
+cd /build && \
+cp /src/miniflux /build/ && \
+cp /src/miniflux.1 /build/ && \
+cp /src/LICENSE /build/ && \
+cp /src/packaging/miniflux.conf /build/ && \
+cp /src/packaging/systemd/miniflux.service /build/debian/ && \
+cp /src/packaging/debian/compat /build/debian/compat && \
+cp /src/packaging/debian/copyright /build/debian/copyright && \
+cp /src/packaging/debian/miniflux.manpages /build/debian/miniflux.manpages && \
+cp /src/packaging/debian/miniflux.postinst /build/debian/miniflux.postinst && \
+cp /src/packaging/debian/rules /build/debian/rules && \
+cp /src/packaging/debian/miniflux.dirs /build/debian/miniflux.dirs && \
+echo "miniflux ($PKG_VERSION) experimental; urgency=low" > /build/debian/changelog && \
+echo "  * Miniflux version $PKG_VERSION" >> /build/debian/changelog && \
+echo " -- Frédéric Guillot <f@miniflux.net>  $PKG_DATE" >> /build/debian/changelog && \
+sed "s/__PKG_ARCH__/${PKG_ARCH}/g" /src/packaging/debian/control > /build/debian/control && \
+dpkg-buildpackage -us -uc -b && \
+lintian --check --color always ../*.deb && \
 cp ../*.deb /pkg/
